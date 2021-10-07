beb615a6d150c1f4f97af065840b3105a1bd7b56
nadia16890 <nadia.aguirre90@gmail.com>
2021-10-06 22:20:48 -0300

    agregadas las entidades faltantes

---
+++ CineORT/Models/Administrador.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CineORT.Models
+{
+    public class Administrador
+    {
+
+        public int Id { get; set; }
+
+        public string Email { get; set; }
+
+        public string Contrasenia { get; set; }
+
+    }
+}
---
+++ CineORT/Models/Funcion.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CineORT.Models
+{
+    public class Funcion
+
+    {
+
+        public string Pelicula { get; set; }
+
+        public int LugaresDisponibles { get; set; }
+
+        public string Fecha { get; set; }
+
+        public string Horario { get; set; }
+
+    }
+}
